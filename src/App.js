8a6894ee19f555d308af1f6402b6ec1b10ce0497
faith kaburu <faith.kaburu@student.moringaschool.com>
2023-09-27 13:07:09 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>faith kaburu</h1>
+          <h1>faith </h1>
         </p>
         <a
           className="App-link"
